fef765889ce72efe269bf62cfb0d195aadb62243
myatejx <myatejx@126.com>
2018-07-16 15:54:17 +0800

    tooltip更新，主界面更新，gradle更新

--- app/src/main/java/com/kingoit/widgetproductor/MainActivity.java
+++ app/src/main/java/com/kingoit/widgetproductor/MainActivity.java
@@ -1,13 +1,34 @@
 package com.kingoit.widgetproductor;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.kingoit.widgetproductor.list.FlowLayoutActivity;
+import com.kingoit.widgetproductor.tip.TooltipActivity;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.to_flow_layout).setOnClickListener(this);
+        findViewById(R.id.to_tooltip).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.to_flow_layout:
+                startActivity(new Intent(MainActivity.this, FlowLayoutActivity.class));
+                break;
+            case R.id.to_tooltip:
+                startActivity(new Intent(MainActivity.this, TooltipActivity.class));
+                break;
+            default:
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/kingoit/widgetproductor/tip/TooltipActivity.java
@@ -0,0 +1,12 @@
+package com.kingoit.widgetproductor.tip;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * @author xmj
+ * @date 2018/7/16
+ */
+public class TooltipActivity extends AppCompatActivity {
+
+
+}
