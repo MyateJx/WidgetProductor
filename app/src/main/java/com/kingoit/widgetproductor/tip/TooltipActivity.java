f994eccd77384f1c210a43c5edbb4a48ebc562da
myatejx <myatejx@126.com>
2018-07-17 10:34:43 +0800

    tooltipActiviy databinding化

--- app/src/main/java/com/kingoit/widgetproductor/tip/TooltipActivity.java
+++ app/src/main/java/com/kingoit/widgetproductor/tip/TooltipActivity.java
@@ -1,46 +1,38 @@
 package com.kingoit.widgetproductor.tip;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
 
 import com.kingoit.tip.tooltip.KingoitTooltip;
 import com.kingoit.widgetproductor.R;
+import com.kingoit.widgetproductor.databinding.ActivityTooltipsBinding;
 
 /**
  * @author xmj
  * @date 2018/7/16
  */
-public class TooltipActivity extends AppCompatActivity implements View.OnClickListener {
+public class TooltipActivity extends AppCompatActivity {
 
-    private Button btnHide, btnShow;
-    private ViewGroup rootlayout;
+    private ActivityTooltipsBinding mBinding;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tooltips);
-        btnHide = findViewById(R.id.btn_hide);
-        btnHide.setOnClickListener(this);
-        btnShow = findViewById(R.id.btn_show);
-        btnShow.setOnClickListener(this);
-        rootlayout = findViewById(R.id.rootLayout);
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_tooltips);
+        mBinding.setClickProxy(new ClickProxy());
     }
 
+    public class ClickProxy {
+        public void toHide() {
+            KingoitTooltip.getInstance().dismissAll();
+        }
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.btn_hide:
-                KingoitTooltip.getInstance().dismissAll();
-                break;
-            case R.id.btn_show:
-                KingoitTooltip.getInstance().showTip(getApplicationContext(), btnHide, rootlayout, "哈哈哈", KingoitTooltip.POSITION_ABOVE);
-                break;
-            default:
+        public void toShow() {
+            KingoitTooltip.getInstance().showTip(getApplicationContext(),
+                    mBinding.btnHide, mBinding.rootLayout, "哈哈哈", KingoitTooltip.POSITION_ABOVE);
         }
     }
+
 }
